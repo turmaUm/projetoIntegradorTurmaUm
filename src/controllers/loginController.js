bdf0f9ded7308b615e8f0d691588c5d3287b7154
Antonio Henrique <antoniohenriqueferreira@outlook.com.br>
2023-03-21 09:21:59 -0300

    Finalizando a função userRegister

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -14,25 +14,30 @@ const loginController = {
         // const senha = req.body.senha
 
         //Segunda forma, usando desestruturação: forma curta
-        const { id, nome, email, senha } = req.body
-
+        const { nome, email, senha } = req.body
+        
         //Dúvidas para o próximo colearning:
         //Entra id na const newUser?
         //Como transformar o campo senha em senhaCriptografada sem alterar o nome 'senha' na desestruturação
         //Duvida para preencher os parametros do campo bcrypt
         
-        let senhaCriptografada = bcrypt.hashSync(newUser.senha, 10); //Para criptografar a senha
-        let novoId = clientes[clientes.length -1].id + 1;
+        let senhaCriptografada = bcrypt.hashSync(senha, 10); //Para criptografar a senha
+        let novoId = 1;
+        
+        if(clientes.length > 0){
+            novoId = clientes[clientes.length -1].id + 1;
+        }
 
         const newUser = { 
             id: novoId,
             nome: nome,
             email: email,
-            senha: senha
+            senha: senhaCriptografada
         }
 
         clientes.push(newUser);
         fs.writeFileSync(path.join(__dirname, '..', '..', 'db', 'clientes.json'), JSON.stringify(clientes, null, 4))
+        res.redirect('/home')
     }
 }
 
