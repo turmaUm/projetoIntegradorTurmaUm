c591655bdcbf49d3c04060c60784f0c14fc90c84
Antonio Henrique <antoniohenriqueferreira@outlook.com.br>
2023-04-06 09:43:21 -0300

    Finalizando CRUD cliente

--- src/controllers/userPanelController.js
+++ src/controllers/userPanelController.js
@@ -1,5 +1,7 @@
 const clientes = require('../../db/clientes.json');
 const bcrypt = require('bcrypt'); 
+const fs = require('fs');
+const path = require('path');
 
 const userPanelController = {
     showCliente: (req, res) => {
@@ -16,19 +18,18 @@ const userPanelController = {
     atualizarPerfil: (req,res) => {
         const { id } = req.params
         const user = clientes.find( user => user.id == id)
-        let senhaComCriptografia = bcrypt.hashSync(senha, 10); 
+        let senhaComCriptografia = bcrypt.hashSync(req.body.nova_senha, 10); 
         //Se encontrar o usuario dentro do arquivo JSON, o 'if' faz a alteração de todos os campos, reescrevendo. 
         if(user != undefined){
             user.nome = req.body.nome
             user.email = req.body.email
             user.telefone = req.body.telefone
             user.enderecos.push(req.body.endereco)
-            user.senha = req.body.nova_senha
+            user.senha = senhaComCriptografia
         }
         // console.log(user);
-
-        clientes.push(user);
         fs.writeFileSync(path.join(__dirname, '..', '..', 'db', 'clientes.json'), JSON.stringify(clientes, null, 4))
+        res.render('cliente/editar-perfil', {user});
     }
 }
 
